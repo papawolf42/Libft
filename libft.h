f9a7fd480377be6f77b55be4a3f9b9cb7fa59d9d
Gunwoo Kim <gunkim@c4r9s1.42seoul.kr>
2020-10-08 17:02:48 +0900

    modified libft.h, detail is below
    delete 18, multiple empty line
    add subfix ft_ to function name

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 16:32:09 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/08 16:50:29 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/08 17:01:48 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 # include <stddef.h>
 
-
 void	*memset(void *b, int c, size_t len);
-void	bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 
-size_t	strlen(const char *s);
+size_t	ft_strlen(const char *s);
 
 #endif
