5aa927aa25a7c33d2913763c9947097d7c163942
김건우 <papawolf@gimgeon-uui-MacBook-Pro.local>
2020-10-09 17:32:29 +0900

    Add concept of casting char to avoid incomplete type

--- ft_memset.c
+++ ft_memset.c
@@ -6,20 +6,23 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 12:21:06 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/08 17:11:01 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/09 17:29:44 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *dest, int c, size_t len)
 {
-	size_t	i;
+	unsigned char	*temp;
+	size_t			i;
 
+	temp = dest;
 	i = 0;
 	while (i < len)
 	{
-		b[i] = (unsigned char)c;
+		temp[i] = (unsigned char)c;
 		i++;
 	}
+	return (temp);
 }
