828f91d80b2a736d6dad488f2b7b1d1cd848aae4
Gunwoo Kim <gunkim@c4r9s1.42seoul.kr>
2020-10-09 20:01:54 +0900

    Implement ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/08 16:32:09 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/09 19:50:48 by gunkim           ###   ########.fr       */
+/*   Created: 2020/10/09 19:22:20 by gunkim            #+#    #+#             */
+/*   Updated: 2020/10/09 19:53:17 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
+void	*ft_memcpy(void *dst, const void *src, size_t len)
+{
+	unsigned char		*bowl_d;
+	const unsigned char	*bowl_s;
+	size_t				i;
 
-void	*ft_memset(void *dest, int c, size_t len);
-void	ft_bzero(void *b, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t len);
-
-size_t	ft_strlen(const char *s);
-
-#endif
+	bowl_d = (unsigned char *)dst;
+	bowl_s = (const unsigned char *)src;
+	i = 0;
+	while (i < len)
+	{
+		bowl_d[i] = bowl_d[i];
+		i++;
+	}
+	return (dst);
+}
