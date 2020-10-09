af1f132951a38ae2498b5c19e6ff11ed209c7e06
Gunwoo Kim <gunkim@c4r9s1.42seoul.kr>
2020-10-09 17:42:43 +0900

    Apply freeBSD man to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 16:32:09 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/08 17:01:48 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/09 17:41:27 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include <stddef.h>
 
-void	*memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *b, size_t len);
 
 size_t	ft_strlen(const char *s);
 
