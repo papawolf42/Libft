6fedefb796b0c8a5675c29bf746347ff517e23c2
김건우 <papawolf@gimgeon-uui-MacBook-Pro.local>
2020-10-10 01:09:50 +0900

    Fix return value and return condition

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 20:04:10 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/09 20:33:08 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/10 01:03:42 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,20 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t len)
 {
 	unsigned char		*bowl_d;
 	const unsigned char *bowl_s;
-	unsigned char		temp_c;
 	size_t				i;
 
-	bowl_d = (unsigned char*)dst;
-	bowl_s = (const unsigned char*)src;
-	temp_c = (unsigned char)c;
+	bowl_d = (unsigned char *)dst;
+	bowl_s = (const unsigned char *)src;
 	i = 0;
 	while (i < len)
 	{
 		bowl_d[i] = bowl_s[i];
-		if (bowl_s[i] == temp_c)
+		if (bowl_s[i] == (unsigned char)c)
 		{
 			i++;
-			break ;
+			return (&dst[i]);
 		}
 		i++;
 	}
-	return (dst);
+	return (NULL);
 }
