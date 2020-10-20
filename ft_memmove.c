1e505100a9576c0ee9b2d408ffd381f2b8cdd302
김건우 <papawolf@kakao.com>
2020-10-21 08:45:27 +0900

    Refactor ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 01:10:27 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/11 02:08:13 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/21 08:45:10 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*c_dst;
-	char	*c_src;
+	unsigned char		*pdst;
+	const unsigned char	*psrc;
 
-	c_dst = (char *)dst;
-	c_src = (char *)src;
-	if (c_dst < c_src)
+	pdst = dst;
+	psrc = src;
+	if (pdst < psrc)
+		while (len--)
+			*pdst++ = *psrc++;
+	else
 		return (ft_memcpy(dst, src, len));
-	else if (c_dst > c_src)
-	{
-		while (len > 0)
-		{
-			c_dst[len - 1] = c_src[len - 1];
-			len--;
-		}
-	}
 	return (dst);
 }
