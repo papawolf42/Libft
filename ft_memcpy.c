cd9ac3e40bc99f4eed852287be5384496475f478
김건우 <papawolf@kakao.com>
2020-10-21 07:21:33 +0900

    Refactor ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 19:22:20 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/10 00:14:32 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/21 07:21:02 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
-	unsigned char		*bowl_d;
-	const unsigned char	*bowl_s;
-	size_t				i;
+	unsigned char		*pdst;
+	const unsigned char	*psrc;
 
-	if (dst == NULL && src == NULL)
+	if (!dst && !src)
 		return (0);
-	bowl_d = (unsigned char *)dst;
-	bowl_s = (const unsigned char *)src;
-	i = 0;
-	while (i < len)
-	{
-		bowl_d[i] = (unsigned char)bowl_s[i];
-		i++;
-	}
+	pdst = dst;
+	psrc = src;
+	while (len--)
+		pdst[len] = psrc[len];
 	return (dst);
 }
