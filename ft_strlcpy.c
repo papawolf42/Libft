aa03ba9599b93f3348354bf86a43cc31db3c1774
김건우 <papawolf@kakao.com>
2020-10-21 11:13:02 +0900

    Refactor ftt_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 04:30:36 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/11 04:59:05 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/21 09:50:48 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
-	if (dst == NULL || src == NULL)
+	if (!dst || !src)
 		return (0);
 	i = 0;
-	if (dstsize != 0)
+	while (src[i] && i + 1 < dstsize)
 	{
-		while (src[i] && i + 1 < dstsize)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	while (src[i])
+		dst[i] = src[i];
 		i++;
-	return (i);
+	}
+	if (dstsize)
+		dst[i] = '\0';
+	return (i + ft_strlen(src + i));
 }
