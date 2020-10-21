626463a491f1e170c524571d3e97a67ab5c39d7e
김건우 <papawolf@kakao.com>
2020-10-21 12:11:05 +0900

    Refactor ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 02:22:10 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/13 19:23:11 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/21 12:10:32 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		len_s;
-	int		i;
-	int		bool_find;
+	char	catch;
+	int		len;
 
-	bool_find = 0;
-	len_s = ft_strlen(s);
-	i = len_s;
-	while (i >= 0)
-	{
-		if (s[i] == (char)c)
-		{
-			bool_find = 1;
-			break ;
-		}
-		i--;
-	}
-	if (bool_find == 0)
-		return (NULL);
-	return ((char *)s + i);
+	if (c == 0)
+		return ((char *)s + ft_strlen(s));
+	catch = c;
+	len = ft_strlen(s);
+	while (len--)
+		if (s[len] == catch)
+			return ((char *)s + len);
+	return (0);
 }
