6a55ed157c323777a801b48b35fd0c99ac518493
김건우 <papawolf@kakao.com>
2020-10-21 09:08:09 +0900

    Refactor ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 03:19:03 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/11 03:38:32 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/21 09:07:43 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	*ft_memchr(const void *b, int c, size_t len)
 {
-	const unsigned char	*str_b;
-	size_t				i;
+	const unsigned char	*ptr;
+	unsigned char		catch;
 
-	str_b = (const unsigned char *)b;
-	i = 0;
-	while (i < len)
+	ptr = b;
+	catch = (unsigned char)c;
+	while (len--)
 	{
-		if (str_b[i] == (unsigned char)c)
-			return ((void *)(str_b + i));
-		i++;
+		if (*ptr == catch)
+			return ((void *)ptr);
+		ptr++;
 	}
 	return (NULL);
 }
