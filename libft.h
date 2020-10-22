5ef4aa6719bbef3068365d9293d1503c300c587c
김건우 <papawolf@kakao.com>
2020-10-22 10:48:05 +0900

    Fix prototype of ft_strlcat

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 16:32:09 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/18 19:19:50 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/22 10:47:22 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void				*ft_memchr(const void *b, int c, size_t len);
 int					ft_memcmp(const void *b1, const void *b2, size_t len);
 size_t				ft_strlen(const char *s);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t				ft_strlcat(char *dst, char *src, size_t dstsize);
+size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *big,
