d711cd0e644bac94b1df4c3381a3e1f7047d3036
김건우 <papawolf@kakao.com>
2020-10-22 11:37:48 +0900

    Refactor ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,64 +6,59 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 21:06:50 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/18 19:25:12 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/22 11:36:02 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		**ft_strscpy(char **strs, const char *s, char c)
+static char		**ft_get_str(char **strs, const char *s, char c)
 {
-	size_t	i;
-	size_t	num;
-	size_t	start;
+	size_t		num;
+	const char	*start;
 
-	i = 0;
 	num = 0;
-	while (s[i])
+	while (*s)
 	{
-		while (s[i] == c && s[i])
-			i++;
-		if (s[i] == '\0')
+		while (*s == c && *s)
+			s++;
+		if (*s == '\0')
 			break ;
-		if (s[i] != c)
-			start = i;
-		while (s[i] != c && s[i])
-			i++;
-		strs[num] = (char *)malloc(sizeof(char) * (i - start + 1));
+		if (*s != c)
+			start = s;
+		while (*s != c && *s)
+			s++;
+		strs[num] = (char *)malloc(sizeof(char) * (s - start + 1));
 		if (!strs[num])
 			return (NULL);
-		ft_strlcpy(strs[num], s + start, i - start + 1);
+		ft_strlcpy(strs[num], start, s - start + 1);
 		num++;
 	}
 	return (strs);
 }
 
-static char		**ft_get_num_blocks(char const *s, char c)
+static char		**ft_get_strs(char const *s, char c)
 {
 	size_t	num;
-	size_t	i;
 	char	**strs;
 
 	num = 0;
-	i = 0;
-	while (s[i])
+	while (*s)
 	{
-		while (s[i] == c && s[i])
-			i++;
-		if (s[i] != c && s[i] != '\0')
+		while (*s == c && *s)
+			s++;
+		if (*s != c && *s)
 			num++;
-		while (s[i] != c && s[i])
-			i++;
+		while (*s != c && *s)
+			s++;
 	}
-	strs = (char **)malloc(sizeof(char *) * (num + 1));
+	strs = (char **)ft_calloc((num + 1), sizeof(char *));
 	if (!strs)
 		return (NULL);
-	strs[num] = NULL;
 	return (strs);
 }
 
-void			ft_free_rest(char **strs)
+void			ft_free_all(char **strs)
 {
 	while (*strs)
 	{
@@ -79,11 +74,11 @@ char			**ft_split(char const *s, char c)
 
 	if (!s)
 		return (NULL);
-	if (!(strs = ft_get_num_blocks(s, c)))
+	if (!(strs = ft_get_strs(s, c)))
 		return (NULL);
-	if (!(ft_strscpy(strs, s, c)))
+	if (!(ft_get_str(strs, s, c)))
 	{
-		ft_free_rest(strs);
+		ft_free_all(strs);
 		return (NULL);
 	}
 	return (strs);
