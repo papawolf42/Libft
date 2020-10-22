b178dbd4c67cf9c04aa878b74d231c45e429518a
김건우 <papawolf@kakao.com>
2020-10-22 14:50:46 +0900

    Remove unnecessary brace

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 07:25:49 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/18 07:33:12 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/10/22 14:49:43 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ t_list	*ft_lstlast(t_list *lst)
 	if (!lst)
 		return (NULL);
 	while (lst->next)
-	{
 		lst = lst->next;
-	}
 	return (lst);
 }
